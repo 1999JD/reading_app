9033cb4c83577c34f1dffb5be4489ffe753f9ad6
張硯喬 <writer159.new@gmail.com>
2021-08-31 02:44:41 +0000

    feat: 設置tailwind.config.js

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="bg-primary">
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <div>
+        <span>username</span>
+      </div>
+    </header>
+    <main>
+      <aside>Sidebar</aside>
+      <nuxt />
+    </main>
+  </div>
+</template>
