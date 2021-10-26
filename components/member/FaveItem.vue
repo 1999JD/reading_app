bc1c7b568c52baacb88beca41eb9e9fbe63d626c
張硯喬 <writer159.new@gmail.com>
2021-10-26 08:33:22 +0000

    feat: 個人專區收藏頁template建置

---
+++ components/member/FaveItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="flex relative">
+    <div class="w-20 h-24 mr-6 bg-gray-300">
+      <img :src="require(`~/assets/icon/home.svg`)" :alt="faveItem.name" />
+    </div>
+    <div>
+      <h3>{{ faveItem.name }}</h3>
+      <p>{{ faveItem.desc }}</p>
+      <div>
+        <p>作者：{{ faveItem.author }}</p>
+        <p>{{ faveItem.publishTime }}出版</p>
+      </div>
+    </div>
+    <div class="absoulte">
+      <img src="" alt="" />
+      <span>收藏</span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'FaveItem',
+  props: {
+    faveItem: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
---
+++ pages/member/favorites.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <ul>
+      <MemberFaveItem
+        v-for="faveItem in favorites"
+        :key="faveItem.id"
+        :fave-item="faveItem"
+      />
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Favorites',
+  data() {
+    return {
+      favorites: [
+        {
+          bookId: 1,
+          imgSrc: '',
+          imgAlt: '',
+          name: '素養人才',
+          desc: '書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容',
+          author: '一路',
+          publishTime: '2021-06-30',
+        },
+        {
+          bookId: 2,
+          imgSrc: '',
+          imgAlt: '',
+          name: '素養人才',
+          desc: '書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容',
+          author: '一路',
+          publishTime: '2021-06-30',
+        },
+        {
+          bookId: 3,
+          imgSrc: '',
+          imgAlt: '',
+          name: '素養人才',
+          desc: '書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容',
+          author: '一路',
+          publishTime: '2021-06-30',
+        },
+        {
+          bookId: 4,
+          imgSrc: '',
+          imgAlt: '',
+          name: '素養人才',
+          desc: '書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容書本小標內容',
+          author: '一路',
+          publishTime: '2021-06-30',
+        },
+      ],
+    }
+  },
+}
+</script>
