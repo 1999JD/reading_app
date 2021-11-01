50e338d7ee6770286486e606f80b07aee153bd83
張硯喬 <writer159.new@gmail.com>
2021-11-01 01:54:57 +0000

    feat: 首頁-熱門，輪播完成

--- components/index/CateHit.vue
+++ components/index/CateHit.vue
@@ -13,53 +13,45 @@
     >
       熱門排行
     </h2>
-    <carousel class="bg-gray-300" :per-page="3">
-      <slide
-        v-for="book in books"
-        :key="book.id"
-        class="
-          box-border
-          self-center
-          flex-shrink-0
-          relative
-          w-44
-          p-2.5
-          pt-4
-          bg-primary
-          rounded
-          shadow-img
-        "
+    <div class="relatvie mb-2.5 overflow-hidden">
+      <ul
+        :style="{ transform: `translateX(${currentTranslate}px)` }"
+        class="slide"
       >
-        <div class="w-28 m-auto">
-          <img
-            :src="require(`~/assets/img/${book.src}.jpg`)"
-            :alt="book.alt"
-            class="shadow-img"
-          />
-        </div>
-
-        <h3
-          :class="[
-            centerItem === book.id ? 'text-base' : 'text-sm',
-            'mt-4 mb-4  text-center',
-          ]"
+        <li
+          v-for="(book, index) in books"
+          :key="book.id"
+          :class="[index === currentIndex ? 'current' : '']"
+          @touchstart.prevent="mousedown(index, $event)"
+          @touchmove.prevent="mousemove"
+          @touchend.prevent="mouseup"
+          @mousedown.prevent="mousedown(index, $event)"
+          @mousemove.prevent="mousemove"
+          @mouseup.prevent="mouseup"
         >
-          {{ book.title }}
-        </h3>
-        <p :class="[centerItem !== book.id ? 'text-xs' : 'text-sm', 'mb-4']">
-          {{ book.intro }}
-        </p>
-        <span class="text-gray-400"> 觀看人數：{{ book.view }} </span>
-      </slide>
-      <!-- <slide> Slide 1 Content </slide>
-      <slide> Slide 2 Content </slide>
-      <slide> Slide 3 Content </slide>
-      <slide> Slide 4 Content </slide> -->
-    </carousel>
-    <!-- <ul
-      ref="scrollContainer"
-      class="flex justify-center gap-x-14 px-6 overflow-x-scroll"
-    ></ul> -->
+          <div class="w-28 m-auto">
+            <img
+              :src="require(`~/assets/img/${book.src}.jpg`)"
+              :alt="book.alt"
+              class="shadow-img"
+            />
+          </div>
+
+          <h3
+            :class="[
+              index === currentIndex ? 'text-base' : 'text-sm',
+              'mt-4 mb-2  text-center',
+            ]"
+          >
+            {{ book.title }}
+          </h3>
+          <p :class="[index !== currentIndex ? 'text-xs' : 'text-sm', 'mb-4']">
+            {{ book.intro }}
+          </p>
+          <span class="text-gray-400 text-xs"> 觀看人數：{{ book.view }} </span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -69,12 +61,17 @@ export default {
 
   data() {
     return {
-      centerItem: 6,
-      scrollLeftNum: 0,
+      isDragging: false,
+      startPos: 0,
+      currentTranslate: 0,
+      prevTranslate: -220,
+      animationID: 0,
+      currentIndex: 0,
+
       books: [
         {
           id: 0,
-          title: '素養人才玲',
+          title: '素養人才',
           intro:
             '我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是......',
           view: 888,
@@ -83,7 +80,7 @@ export default {
         },
         {
           id: 1,
-          title: '素養人才一',
+          title: '素養人才',
           intro:
             '我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是......',
           view: 888,
@@ -92,7 +89,7 @@ export default {
         },
         {
           id: 2,
-          title: '素養人才二',
+          title: '素養人才',
           intro:
             '我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是......',
           view: 888,
@@ -101,7 +98,7 @@ export default {
         },
         {
           id: 3,
-          title: '素養人才三',
+          title: '素養人才',
           intro:
             '我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是......',
           view: 888,
@@ -110,7 +107,7 @@ export default {
         },
         {
           id: 4,
-          title: '素養人才四',
+          title: '素養人才',
           intro:
             '我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是......',
           view: 888,
@@ -118,17 +115,60 @@ export default {
           alt: '素養人才',
         },
       ],
-      width: 0,
     }
   },
-  created() {},
-  mounted() {},
-  methods: {},
+  created() {
+    this.currentIndex = 3
+    this.currentTranslate = (this.currentIndex - 1) * -216 + -222 + 99
+  },
+  methods: {
+    mousedown(index, event) {
+      this.isDragging = true
+      this.currentIndex = index
+      this.startPos = this.getPositionX(event)
+      this.animationID = requestAnimationFrame(this.animation)
+    },
+    mousemove(event) {
+      if (this.isDragging) {
+        const currentPosition = this.getPositionX(event)
+        this.currentTranslate =
+          this.prevTranslate + currentPosition - this.startPos
+      }
+    },
+    mouseup() {
+      cancelAnimationFrame(this.animationID)
+      this.isDragging = false
+      const movedBy = this.currentTranslate - this.prevTranslate
+      if (movedBy < -100 && this.currentIndex < this.books.length - 1)
+        this.currentIndex += 1
+      if (movedBy > 100 && this.currentIndex > 0) this.currentIndex -= 1
+      this.setPositionByIndex()
+    },
+    animation() {
+      if (this.isDragging) requestAnimationFrame(this.animation)
+    },
+    getPositionX(event) {
+      return event.type.includes('mouse')
+        ? event.pageX
+        : event.touches[0].clientX
+    },
+    setPositionByIndex() {
+      this.currentTranslate = (this.currentIndex - 1) * -216 + -222 + 99
+      this.prevTranslate = this.currentTranslate
+    },
+  },
 }
 </script>
 
-<style>
-.VueCarousel-inner {
-  gap: 56px;
+<style scoped>
+.slide {
+  @apply box-border flex items-center transition-all;
+}
+.slide li {
+  @apply box-border self-center flex-shrink-0 relative w-40 mx-7 p-2.5 pt-4 bg-primary rounded shadow-img;
+}
+
+.slide .current {
+  @apply w-44;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {
-      src: '~/plugins/vue-carousel.js', mode: 'client'
-    },
     { src: '~/plugins/axios.js', ssr: true },
   ],
 
