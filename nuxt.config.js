636cb010f750bde2e9ae23a6ba93c99e2c50f98b
張硯喬 <writer159.new@gmail.com>
2021-11-19 01:56:41 +0000

    feat: 使用 proxy 做反向代理

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -18,22 +17,17 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
   },
-
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/css/animation.css'
   ],
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/axios.js', ssr: true },
     { src: '~/plugins/yup.js', ssr: true }
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
@@ -43,7 +37,6 @@ export default {
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
@@ -52,7 +45,6 @@ export default {
     '@nuxtjs/pwa',
     '@nuxtjs/auth-next'
   ],
-
   auth: {
     redirect: {
       login: '/account/login',
@@ -76,11 +68,11 @@ export default {
         },
         user: {
           property: 'user',
-          autoFetch: true
+          autoFetch: true,
         },
         endpoints: {
           login: {
-            url: '/auth/login',
+            url: 'auth/login',
             method: 'post'
           },
           refresh: {
@@ -103,36 +95,29 @@ export default {
       ssr: true
     }]
   },
-
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseUrl:
-      process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3000/api/' : 'http://127.0.0.1:3000/api/',
-    // proxy: true,
-    credentials: true,
-    retry: { retires: 3 }
+    proxy: true,
+    prefix: '/api',
   },
-
   proxy: {
     '/api': {
       target:
         process.env.NODE_ENV === 'development'
           ? 'http://127.0.0.1:3000/api/'
           : 'http://127.0.0.1:3000/api/',
-      // pathRewrite: { "^/api/": "" }
+      pathRewrite: { '^/api': '/' },
     },
   },
-
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en'
     }
   },
-
   serverMiddleware: [
     { path: '/api', handler: '~/server/app.js' }
   ],
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend(config) {
@@ -154,20 +139,16 @@ export default {
       }
     },
   },
-
   video: ['src', 'poster'],
   source: 'src',
   audio: 'src',
   img: 'src',
   image: ['xlink:href', 'href'],
   use: ['xlink:href', 'href'],
-
   loading: {
     color: 'Black',
     height: '4px',
     continuous: true,
     duration: 3000
   }
-
-
 }
